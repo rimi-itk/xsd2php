25291bf3aeb34fa99b1020f11bcddef587386472
Mikkel Ricky <rimi@aarhus.dk>
2023-01-19 21:47:20 +0100

    Generate unique case-insensitive paths

--- src/Jms/PathGenerator/Psr4PathGenerator.php
+++ src/Jms/PathGenerator/Psr4PathGenerator.php
@@ -7,6 +7,8 @@ use GoetasWebservices\Xsd\XsdToPhp\PathGenerator\Psr4PathGenerator as Psr4PathGe
 
 class Psr4PathGenerator extends Psr4PathGeneratorBase implements PathGenerator
 {
+    private $paths = [];
+
     public function getPath($yaml)
     {
         $ns = key($yaml);
@@ -19,8 +21,24 @@ class Psr4PathGenerator extends Psr4PathGeneratorBase implements PathGenerator
                     throw new PathGeneratorException("Can't create the folder '$dir'");
                 }
                 $f = trim(strtr(substr($ns, strlen($namespace)), '\\/', '..'), '.');
+                $path = $dir . '/' . $f . '.yml';
 
-                return $dir . '/' . $f . '.yml';
+                // Make sure that we generate a unique case-insensitive path.
+                // Otherwise we'll into problems on case-insensitive file systems.
+                $normalizedPath = strtolower($path);
+                if (isset($this->paths[$normalizedPath])) {
+                    for ($i = 0; $i < 100; $i++) {
+                        $uniquePath = preg_replace('@\.yml$@', '-' . $i . '.yml', $path);
+                        if (!isset($this->paths[strtolower($uniquePath)])) {
+                            $path = $uniquePath;
+                            $normalizedPath = strtolower($path);
+                            break;
+                        }
+                    }
+                }
+                $this->paths[$normalizedPath] = $path;
+
+                return $path;
             }
         }
         throw new PathGeneratorException("Unable to determine location to save JMS metadata for class '$ns'");
